82cdbdfc3a42a2dfb48b49e42300e6bdc26bf7b3
Rizky Alfiansyah <Ralfiansyah111@gmail.com>
2021-09-22 11:01:34 +0700

    Chapter 5 Angry Bird

---
+++ Assets/Scripts/YellowBird.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Bird;
+
+public class YellowBird : Bird
+{
+    [SerializeField] public float _boostForce = 100;
+    [SerializeField] public bool _hasBoost = false;
+
+
+    public void Boost()
+    {
+        if (State == BirdState.Thrown && !_hasBoost)
+        {
+            RigidBody.AddForce(RigidBody.velocity * _boostForce);
+            _hasBoost = true;
+        }
+    }
+
+    public override void OnTap()
+    {
+        Boost();
+    }
+}
